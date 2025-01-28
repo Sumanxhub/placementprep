0752ac0fd3816c33b14bfd49dca9b7be87bc2abe
Sumanxhub <sumanbh0329@gmail.com>
2025-01-28 23:55:05 +0000

    improved test of climbing stairs

--- Leetcode/ClimbingStairs/test.cpp
+++ Leetcode/ClimbingStairs/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 int climbStairs(int n) {
   int a = 2, b = 3, r;
   if (n < 4) {
@@ -18,12 +19,20 @@ int climbStairs(int n) {
 }
 
 int main() {
-  int num;
-  cout << "Enter number: ";
-  cin >> num;
-  cout << "\nResult: ";
-  cout << climbStairs(num);
-  cout << "\n";
+  // Test Case 1
+  int num1 = 4;
+  cout << "Number of Stairs: " << num1 << endl;
+  cout << "Possible ways to climb stairs: " << climbStairs(num1) << endl;
+
+  // Test Case 2
+  int num2 = 8;
+  cout << "\nNumber of Stairs: " << num2 << endl;
+  cout << "Possible ways to climb stairs: " << climbStairs(num2) << endl;
+
+  // Test Case 3
+  int num3 = 2;
+  cout << "\nNumber of Stairs: " << num3 << endl;
+  cout << "Possible ways to climb stairs: " << climbStairs(num3) << endl;
 
   return 0;
 }
